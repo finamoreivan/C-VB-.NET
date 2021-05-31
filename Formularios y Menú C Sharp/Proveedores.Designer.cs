19f9855013beb2542b21452ce33f80866a004b03
Iván Finamore <66212164+finamoreivan@users.noreply.github.com>
2021-05-31 14:59:18 -0300

    Add files via upload

---
+++ Formularios y Menú C Sharp/Proveedores.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Formulaarios_y_Menú_C_Sharp
+{
+    partial class Proveedores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bVolver = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bVolver
+            // 
+            this.bVolver.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.bVolver.Location = new System.Drawing.Point(12, 307);
+            this.bVolver.Name = "bVolver";
+            this.bVolver.Size = new System.Drawing.Size(75, 23);
+            this.bVolver.TabIndex = 1;
+            this.bVolver.Text = "Volver";
+            this.bVolver.UseVisualStyleBackColor = false;
+            this.bVolver.Click += new System.EventHandler(this.bVolver_Click);
+            // 
+            // Proveedores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 342);
+            this.Controls.Add(this.bVolver);
+            this.Name = "Proveedores";
+            this.Text = "Proveedores";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bVolver;
+    }
+}
